7cdb7e50ce713eb1cf2d438a8be3130edc87b147
eshamster <hamgoostar@gmail.com>
2019-06-09 14:09:30 +0000

    Update: Set FPS to 30
    But frequency of update of drawing is half of it for safety.

--- game/process.lisp
+++ game/process.lisp
@@ -3,10 +3,13 @@
   (:export :update-game
            :init-game)
   (:import-from :cl-csr-jintori/game/state/package
-                :init-cl-csr-jintori-state))
+                :init-cl-csr-jintori-state)
+  (:import-from :cl-csr-2d-game
+                :set-update-frequency))
 (in-package :cl-csr-jintori/game/process)
 
 (defun init-game ()
+  (set-update-frequency 30 2)
   (init-cl-csr-jintori-state))
 
 (defun update-game ())
