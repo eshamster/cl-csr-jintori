599a88605477db8f1d45ea52c029ffb55afb9f4a
eshamster <hamgoostar@gmail.com>
2019-08-15 12:58:16 +0000

    Fix: Enable admin console when starting server

--- main.lisp
+++ main.lisp
@@ -1,6 +1,7 @@
 (defpackage cl-csr-jintori/main
   (:nicknames :cl-csr-jintori)
   (:use :cl
-        :cl-csr-jintori/server)
+        :cl-csr-jintori/server
+        :cl-csr-jintori/admin/admin)
   (:export :start-game
            :stop-game))
